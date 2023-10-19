c60c65a62a39312f5a532ba2aa69502df956263a
System Administrator <pr1206@gmail.com>
2023-10-18 22:36:13 -0400

    moved index.html to root

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from './ErrorBoundary';
+import './containers/App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      cats: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({cats: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
+
+  render(){
+    const {cats, searchfield} = this.state
+    const filteredRobots = cats.filter(cat => {
+      return cat.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    if (!cats.length){
+      return <h1>Loading</h1>
+    }else{
+      return (
+        <div className = 'tc'>
+          <h1>CatFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      );
+    } 
+  }
+}
+
+export default App;
